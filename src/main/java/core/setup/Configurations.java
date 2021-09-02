e05ea8175556bec452b69b20abae19f08bd45f90
Aliaksandr-Drabkou <Aliaksandr.Drabkou@itechart-group.com>
2021-09-02 10:41:52 +0300

    add cloud capabillities
    refactor Driver class

--- src/main/java/core/setup/Configurations.java
+++ src/main/java/core/setup/Configurations.java
@@ -1,5 +1,7 @@
 package core.setup;
 
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,10 +13,30 @@ import java.util.logging.Logger;
 public class Configurations {
     protected final Logger log = Logger.getLogger("AD");
     protected final String osName = getProperty("OS");
+    protected final String isRemote = getProperty("isRemote");
     protected final String application = getFile(getProperty("application")).getAbsolutePath();
     protected final String deviceName = getProperty("deviceName");
     protected final String iOsPlatformVersion = getProperty("iOsPlatformVersion");
     protected final String androidPlatformVersion = getProperty("androidPlatformVersion");
+    protected final String browserStackUser = getProperty("browserStackUser");
+    protected final String browserStackKey = getProperty("browserStackKey");
+    protected final String browserStackAppUrl = getProperty("browserStackAppUrl");
+    protected final String browserStackDeviceName = getProperty("browserStackDeviceName");
+    protected final String cloudUrl = getProperty("cloudURL");
+
+    protected DesiredCapabilities commonCapabilities(String osName, String platformVersion, String deviceName, String application) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        if (isRemote.equals("true")) {
+            capabilities.setCapability("browserstack.user", browserStackUser);
+            capabilities.setCapability("browserstack.key", browserStackKey);
+        }
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, osName);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, platformVersion);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+        capabilities.setCapability(MobileCapabilityType.APP, application);
+        return capabilities;
+    }
 
     private String getProperty(String propertyName) {
         Properties property = new Properties();
